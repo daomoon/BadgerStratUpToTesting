e12042e57fda63a72f0bdbfa81d8da5f68d0ee32
Stargarden <stargardenbeats@gmail.com>
2021-07-04 12:37:37 -0700

    Leverage function working - must still add to leverage() - tend() call.

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -37,7 +37,7 @@ contract MyStrategy is BaseStrategy {
     address public constant ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     address public constant AAVE_TOKEN = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
     address public constant WETH_TOKEN = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address public constant GUSD = 0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     function initialize(
         address _governance,
@@ -50,7 +50,7 @@ contract MyStrategy is BaseStrategy {
     ) public initializer {
         __BaseStrategy_init(_governance, _strategist, _controller, _keeper, _guardian);
 
-        /// @dev Add config here
+        /// @dev Add config here for GUSD
         want = _wantConfig[0];
         aToken = _wantConfig[1];
         reward = _wantConfig[2];
@@ -64,6 +64,7 @@ contract MyStrategy is BaseStrategy {
 
         IERC20Upgradeable(reward).safeApprove(ROUTER, type(uint256).max);
         IERC20Upgradeable(AAVE_TOKEN).safeApprove(ROUTER, type(uint256).max);
+        IERC20Upgradeable(USDC).safeApprove(ROUTER, type(uint256).max);
 
     }
 
@@ -169,14 +170,45 @@ contract MyStrategy is BaseStrategy {
         return _amount;
     }
 
+    // @dev Returns the maximum amount of _token that should be borrowed
     function getMaxBorrow(address _token) public view returns (uint256) {
-        uint256 borrowLimit = getAvailableBorrows().mul(4).div(getPrice(_token).mul(5)).mul(100);
+        uint256 borrowLimit = getAvailableBorrows().mul(4).div(getPrice(_token).mul(5)).mul(10 ** 6);
         return borrowLimit;
     }
 
+    // @dev Borrows the amount returned by getMaxBorrow
     function maxBorrow(address _token) external whenNotPaused {
         _onlyAuthorizedActors();
         ILendingPool(LENDING_POOL).borrow(_token, getMaxBorrow(_token), 2, 0, address(this));
+    }
+
+    // @dev Swaps from borrowed token to want, deposits and repeats until borrowed amount is too small
+    // @dev This function is still missing getConfiguration() call from LendingPool to enable other non-GUSD tokens
+    function leverage() external whenNotPaused {
+        _onlyAuthorizedActors();
+        if (getMaxBorrow(USDC) >= 2000 * 10 ** 6) {
+
+            uint256 _borrowedAmount = IERC20Upgradeable(USDC).balanceOf(address(this));
+
+            ISwapRouter.ExactInputSingleParams memory fromBorrowedToWantParams = ISwapRouter.ExactInputSingleParams(
+                USDC,
+                want,
+                10000,
+                address(this),
+                now,
+                _borrowedAmount,
+                0,
+                0
+            );
+
+            ISwapRouter(ROUTER).exactInputSingle(fromBorrowedToWantParams);
+
+        }
+    }
+
+    function getBorrowedAmount() public view returns (uint256) {
+        uint256 _borrowedAmount = IERC20Upgradeable(USDC).balanceOf(address(this));
+        return _borrowedAmount;
     }
 
     /// @dev Harvest from strategy mechanics, realizing increase in underlying position
